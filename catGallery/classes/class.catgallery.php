23724e34aa4cc55d3989b2df739688c8dfe89b5c
Matthias Glienke <support@creativecat.de>
2015-01-23 20:52:44 +0100

    Added url to original image

--- catGallery/classes/class.catgallery.php
+++ catGallery/classes/class.catgallery.php
@@ -513,6 +513,7 @@ if ( ! class_exists( 'catGallery', false ) ) {
 						'image_id'			=> $row['image_id'],
 						'position'			=> $row['position'],
 						'picture'			=> $row['picture'],
+						'original'			=> $this->getFolder(false) . '/' . $row['picture'],
 						'options'			=> $addOptions ? $this->getImgOptions( $row['image_id'] ) : NULL,
 						'image_content'		=> $addContent ? $this->getImgContent( $row['image_id'] ) : NULL,
 						'contentname'		=> 'image_content_' . $row['image_id'],
